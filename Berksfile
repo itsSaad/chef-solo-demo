1428a5b10350f5372ea7269bb2d6eb029f8c53bf
Saad Masood <itssaad@icloud.com>
2016-06-19 19:08:28 +0500

    selinux permissive mode
    selinux permissive mode

--- Berksfile
+++ Berksfile
@@ -1,1 +1,4 @@
 source "https://api.berkshelf.com"
+
+cookbook 'selinux', '~> 0.9.0'
+cookbook 'mysql', '~> 7.1.1'
--- site-cookbooks/mysql_wrapper/metadata.rb
+++ site-cookbooks/mysql_wrapper/metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures mysql_wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'selinux', '~> 0.9.0'
 depends 'mysql', '~> 7.1.1'
